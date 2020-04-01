cc3f7ed51c0f52555374e076f8e4abaf78322a5b
Connor McCormack <mccormack.connor.m@gmail.com>
2020-04-01 14:13:17 -0500

    first local commit

--- src/blogful.js
+++ src/blogful.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const knex = require('knex')
 const ArticlesService = require('./articles-service')
 
+console.log(process.env.DB_URL)
+
 const knexInstance = knex({
     client: 'pg',
     connection: process.env.DB_URL,
 })
 
-console.log(ArticlesService.getAllArticles())
+knexInstance('blogful_articles')
+    .select('*')
+    .then(console.log)
+
+// console.log(ArticlesService.getAllArticles())
