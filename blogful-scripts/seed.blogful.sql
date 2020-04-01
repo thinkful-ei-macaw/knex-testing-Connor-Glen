92b67f63663ffa3bfdba07e0f90d365505a6a6fb
Alex Larrabee <nekotree556@gmail.com>
2020-04-01 11:29:38 -0700

    now with testing!

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,7 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Article 1', '2016-06-22 19:10:25-07','Content 1'),
+    ('Article 2', '2017-06-22 20:11:25-08','Content 2'),
+    ('Article 3', '2018-06-22 21:12:25-09','Content 3'),
+    ('Article 4', '2019-06-22 22:13:25-10','Content 4')
+;
